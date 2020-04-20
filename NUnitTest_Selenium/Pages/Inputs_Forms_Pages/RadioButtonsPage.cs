23772b24f57d579fb33293acabe886615defaf86
Vesturcikas <vestas1976@gmail.com>
2020-04-20 17:29:11 +0300

    Changes in the project structure

---
+++ NUnitTest_Selenium/Pages/Inputs_Forms_Pages/RadioButtonsPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnitTest_Selenium.Pages;
+using OpenQA.Selenium;
+
+
+namespace NUnitTest_Selenium.Pages
+{
+    public class RadioButtonsPage :BasePage
+    {
+        private IWebElement topMaleRadioButton => driver.FindElement(By.CssSelector("input[value='Male']"));
+        private IWebElement topFemaleRadioButton => driver.FindElement(By.CssSelector("input[value='Female']"));
+        private IWebElement buttonGetValue => driver.FindElement(By.Id("buttoncheck"));
+
+        public RadioButtonsPage(IWebDriver driver) : base(driver) { }
+
+        public RadioButtonsPage TopMaleButtonClick()
+        {
+
+            return this;
+        }
+    }
+}
